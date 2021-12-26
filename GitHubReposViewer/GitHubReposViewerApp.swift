bc7d846006ddfb3be36b0e1d2cb62b313a41f0cc
Svyat Zubyak <svyat.zubyak@gmail.com>
2021-12-26 09:44:03 +0200

    Initial Commit

---
+++ GitHubReposViewer/GitHubReposViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubReposViewerApp.swift
+//  GitHubReposViewer
+//
+//  Created by Svyat Zubyak on 26.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct GitHubReposViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
